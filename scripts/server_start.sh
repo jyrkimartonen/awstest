e695de3d43d9b2db2383e22cad658ccfdae986ef
Jyrki Martonen <jyrki.martonen@kooditutorit.fi>
2022-12-02 12:57:50 +0200

    scripts

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -DenvTarget=prod -Dsome=stuff *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -DenvTarget=prod -Dsome=stuff -Dserver.port=80 -jar *.jar > /dev/null 2> /dev/null < /dev/null &
