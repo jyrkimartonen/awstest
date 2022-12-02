d51180a1ae660248e474885403faaa83e72507ae
Jyrki Martonen <jyrki.martonen@kooditutorit.fi>
2022-12-02 12:12:29 +0200

    scripts

--- src/main/java/net/tutorit/awstest/AwsTestApplication.java
+++ src/main/java/net/tutorit/awstest/AwsTestApplication.java
@@ -3,6 +3,10 @@ package net.tutorit.awstest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 @PropertySource({ "classpath:application-${envTarget:dev}.properties" })
--- src/main/java/net/tutorit/awstest/controllers/HelloController.java
+++ src/main/java/net/tutorit/awstest/controllers/HelloController.java
@@ -4,9 +4,13 @@
  */
 package net.tutorit.awstest.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
