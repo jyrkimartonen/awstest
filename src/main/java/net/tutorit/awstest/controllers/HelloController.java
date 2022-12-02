01582172d028d58051afc70b0929512d9c1089ba
Jyrki Martonen <jyrki.martonen@kooditutorit.fi>
2022-12-02 15:03:58 +0200

    scripts

--- src/main/java/net/tutorit/awstest/controllers/HelloController.java
+++ src/main/java/net/tutorit/awstest/controllers/HelloController.java
@@ -44,7 +44,7 @@ public class HelloController {
         ret+="From environment variables of the system (through the injected Environment-object)<br />";
         ret+="Database: "+environment.getProperty("my_database")+"<br/><br />";
         ret+="From virtual machine command line parameters (-Dsome=xxxx) <br />";
-        ret+="some: "+some+"<br /><br />";
+        ret+="some: "+some+"<br />Jotain muuta<br />";
         return ret;
     }
 }
