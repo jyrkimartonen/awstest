c626fd55f407d040525b6bd5159ba359ec50c4f0
Jyrki Martonen <jyrki.martonen@kooditutorit.fi>
2022-12-02 19:47:03 +0200

    Env

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+sudo pkill -f 'java -jar'
 sudo rm -rf /home/ec2-user/server
--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -DenvTarget=prod -Dsome=stuff -Dserver.port=80 -jar *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -cp . -DenvTarget=prod -Dsome=stuff -jar *.jar > /dev/null 2> /dev/null < /dev/null &
